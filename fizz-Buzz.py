d7448e6898a7ae8645d067a29d19a3dc2e0be8a3
Geleta116 <104783472+Geleta116@users.noreply.github.com>
2022-08-17 00:33:24 +0300

    Add files via upload

---
+++ fizz-Buzz.py
@@ -0,0 +1,20 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        lst = []
+
+        for j in range (1,n+1):
+            lst.append(j)
+
+
+   
+        for i in range(len(lst)):
+            if lst[i]%3==0 and lst[i]%5 != 0:
+                lst[i] = "Fizz"
+            elif lst[i]%3!=0 and lst[i]%5== 0:
+                lst[i] = "Buzz"
+            elif lst[i]%3==0 and lst[i]%5== 0:
+                lst[i] ="FizzBuzz" 
+            elif lst[i]%3!=0 and lst[i]%5!= 0:    
+                lst[i] = str(lst[i])
+    
+        return lst
