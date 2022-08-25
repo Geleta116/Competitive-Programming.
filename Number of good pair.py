6235497ffd5c0ee9b37d535da56895332a17d591
Geleta116 <104783472+Geleta116@users.noreply.github.com>
2022-08-25 18:49:52 +0300

    Update Number of good pair.py

--- Number of good pair.py
+++ Number of good pair.py
@@ -1,11 +1,11 @@
-nums = [1,2,3]
-num_good_pair = 0
-for i in range(len(nums)):
-    j = 0
-    while j<len(nums):
-        if i<j:
-            if nums[i] == nums[j]:
-                num_good_par+=1
-        j+=1
-print(good_pair)
-            
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        num_good_pair = 0
+        for i in range(len(nums)):
+            j = 0
+            while j<len(nums):
+                if i<j:
+                    if nums[i] == nums[j]:
+                        num_good_pair+=1
+                j+=1
+        return(num_good_pair)
