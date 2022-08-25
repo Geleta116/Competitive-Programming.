03745566ab4f0a9756bf5d914fa4588967aa3076
Geleta116 <104783472+Geleta116@users.noreply.github.com>
2022-08-25 18:29:40 +0300

    Leet Code

---
+++ K Closest Points to Origin.py
@@ -0,0 +1,19 @@
+import math
+def kClosest(points,k):
+    distance = []
+    for v in range(len(points)):
+        i = points[v]
+        r = i[0] ** 2 + i[1] ** 2
+        distance.append(math.sqrt(r))
+        
+    
+    dictionary = [dictionary for distance, dictionary in sorted(zip(distance,points))]
+    Final = []
+    for index in range(k):
+        Final.append(dictionary[index])
+    print(Final)
+    
+  
+
+kClosest([[1,3],[-2,2],[0,0],[0,0],[-2,-2]],0)
+
