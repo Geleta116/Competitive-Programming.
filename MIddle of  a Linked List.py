5863e4cac03f466b527077ced7a1b72cf85ce227
Geleta116 <104783472+Geleta116@users.noreply.github.com>
2022-09-06 16:14:01 +0300

    Leet Code

---
+++ MIddle of  a Linked List.py
@@ -0,0 +1,47 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        length = 0
+        nodes = head
+        while nodes!=None:
+            length+=1
+            nodes = nodes.next
+            
+        i = length//2
+        index = 0
+        output = head
+        while output!=None:
+            if index == i:
+                return output
+            else:
+                index+=1
+                output = output.next
+
+'''
+I tried the below solution and it also works
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        length = 0
+        nodes = head
+        while nodes!=None:
+            length+=1
+            nodes = nodes.next
+        i = 0
+        j = 0
+        while j<length-1:
+            j+=2
+            i+=1
+        index = 0
+        output = head
+        while output!=None:
+            if index == i:
+                return output
+            else:
+                index+=1
+                output = output.next
+'''
+    
