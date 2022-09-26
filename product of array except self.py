83a7e130ca60ed67beda286475714d315c2c4189
Geleta116 <104783472+Geleta116@users.noreply.github.com>
2022-09-26 10:09:15 +0300

    Update product of array except self.py

--- product of array except self.py
+++ product of array except self.py
@@ -30,3 +30,23 @@ class Solution:
             else:
                 out.append(pre[a-1]*post[a+1])
         return out
+    
+    '''
+    I have also tried the below solution and it got accepted
+    class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        pre = 1
+        post = 1
+        out = [1]
+        for i in range(len(nums)-1):
+            out.append(pre*nums[i])
+            pre = out[i+1]
+        
+        for j in range(len(out)-1,-1,-1):
+            out[j] = post * out[j]
+            post = post*nums[j]
+        return out
+            
+        
+            
+    '''
