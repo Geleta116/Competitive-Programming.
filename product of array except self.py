714d1578bb75aac6529e421508e101f0e25ff3f6
Geleta116 <104783472+Geleta116@users.noreply.github.com>
2022-09-26 09:51:48 +0300

    Leet code

---
+++ product of array except self.py
@@ -0,0 +1,32 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        pre = [1]
+        for i in range(len(nums)):
+            pre.append(pre[i]*nums[i])
+        pre = pre[1:]
+        post = [1]
+        l = 0
+        m = len(nums)-1
+        while l<m:
+            nums[l],nums[m]=nums[m],nums[l]
+            l+=1
+            m-=1    
+        for j in range(len(nums)):
+            post.append(post[j]*nums[j])
+        post = post[1:]
+         
+        k = 0
+        c = len(post)-1
+        while k<c:
+            post[k],post[c]=post[c],post[k]
+            k+=1
+            c-=1 
+        out = []
+        for a in range(len(pre)):
+            if a == 0:
+                out.append(1*post[a+1])
+            elif a ==  len(pre)-1:
+                out.append(pre[a-1]*1)
+            else:
+                out.append(pre[a-1]*post[a+1])
+        return out
