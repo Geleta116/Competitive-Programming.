d9b3608ef7e5e8fc9f6420fdcd7da02e1f967ffd
Geleta116 <104783472+Geleta116@users.noreply.github.com>
2022-10-02 10:29:35 +0300

    Leet code

---
+++ Insertion Sort List.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        dummy.next = head
+        insert = head
+        while head and head.next:
+            if head.val > head.next.val:
+                insert = head.next
+                sorted_list = dummy
+                while sorted_list.next.val<insert.val:
+                    sorted_list = sorted_list.next
+                head.next = insert.next
+                insert.next = sorted_list.next
+                sorted_list.next = insert   
+            else:
+                head= head.next
+                
+        return dummy.next
+        
