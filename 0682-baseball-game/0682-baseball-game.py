9796c5fb843601ca393c563bdf9ba7db4e3699a2
Geleta116 <104783472+Geleta116@users.noreply.github.com>
2022-10-10 20:35:29 +0300

    Time: 55 ms (78.46%), Space: 14.1 MB (32.01%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.py
@@ -0,0 +1,21 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        st = []
+        for i in operations:
+            if i=='C':
+                st.pop()
+                
+            elif i=="D" :
+                p1 = st.pop()
+                st.append(p1)
+                st.append(2*p1)
+                  
+            elif i=='+':
+                st.append(st[-1]+st[-2])        
+            else:
+                st.append(int(i))
+        
+        return sum(st)
+            
+       
+        
