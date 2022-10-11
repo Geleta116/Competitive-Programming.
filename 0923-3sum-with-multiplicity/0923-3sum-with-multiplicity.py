a7ea74c884f70d6af40c80d657a5ec3af9885282
Geleta116 <104783472+Geleta116@users.noreply.github.com>
2022-10-11 15:37:57 +0300

    Time: 74 ms (98.68%), Space: 14.1 MB (55.57%) - LeetHub

---
+++ 0923-3sum-with-multiplicity/0923-3sum-with-multiplicity.py
@@ -0,0 +1,35 @@
+class Solution:
+    def threeSumMulti(self, arr: List[int], target: int) -> int:
+        count = {}
+        for l in arr:
+            if l in count:
+                count[l]+=1
+            else:
+                count[l]=1
+        #I could have used the Counter function 
+        temp_dic = sorted(count.keys())
+        out = 0
+        for i,n in enumerate(temp_dic):
+            j = i 
+            k = len(temp_dic)-1
+            temp_targ = target - n
+            while j<=k:
+                
+                if temp_dic[j]+temp_dic[k]<temp_targ:
+                    j+=1
+                elif temp_dic[j]+temp_dic[k]>temp_targ:
+                    k-=1
+                else:
+                    if i<j<k:
+                        out += count[temp_dic[i]]*count[temp_dic[j]]*count[temp_dic[k]]
+                    elif i==j<k:
+                        out += (count[temp_dic[i]]*(count[temp_dic[i]]-1))//2 * count[temp_dic[k]]
+                    elif i<j==k:
+                        out += count[temp_dic[i]]*(count[temp_dic[j]]*(count[temp_dic[j]]-1))//2
+                    else:
+                        out += (count[temp_dic[i]]*(count[temp_dic[j]]-1)*(count[temp_dic[k]]-2))//6
+                    j+=1
+                    k-=1
+        return out%(10**9 + 7)
+                    
+        
