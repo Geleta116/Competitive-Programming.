f95c1731abf67f7870ad7c1f5c8eae5e6aad6e73
Geleta116 <104783472+Geleta116@users.noreply.github.com>
2022-11-06 01:31:25 +0300

    Time: 260 ms (63.18%), Space: 14 MB (87.73%) - LeetHub

---
+++ 0481-magical-string/0481-magical-string.py
@@ -0,0 +1,12 @@
+class Solution:
+    def magicalString(self, n: int) -> int:
+        
+        s = "122"
+        for i in range(2,n):
+            if s[-1]=="2":
+                s+= "1"*int(s[i])
+            else:
+                s+= "2"*int(s[i])
+            if len(s)>n:
+                break
+        return s[:n].count("1")
