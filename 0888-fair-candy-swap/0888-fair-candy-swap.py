7c6d7f7c8bd983f688be210c00129bb9e3efa50f
Geleta116 <104783472+Geleta116@users.noreply.github.com>
2022-11-06 23:31:51 +0300

    Time: 505 ms (82.04%), Space: 16.9 MB (20.33%) - LeetHub

--- 0888-fair-candy-swap/0888-fair-candy-swap.py
+++ 0888-fair-candy-swap/0888-fair-candy-swap.py
@@ -3,13 +3,11 @@ class Solution:
         sum_x = sum(aliceSizes)
         sum_y = sum(bobSizes)
         dif = (sum_x - sum_y)//2
-        for item in range(len(bobSizes)):
-            bobSizes[item] += dif
-        temp = []
-        print(bobSizes)
-        for new_items in aliceSizes:
-            if new_items in bobSizes:
-                return [new_items,new_items-dif]
-        
+        x = set(aliceSizes)
+        y = set(bobSizes)
+        for i in x:
+            if i - dif in y:
+                return  [ i,i-dif]
+                
         
         
