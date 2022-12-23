9332b24b7cd219599fc2d9e98696f09f0270f202
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2022-12-23 15:46:20 +0300

    Time: 850 ms (74.24%), Space: 19.4 MB (37.85%) - LeetHub

--- 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.py
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.py
@@ -1,8 +1,6 @@
 class Solution:
     def nearestValidPoint(self, x: int, y: int, points: List[List[int]]) -> int:
         out = float(inf)
-        def helper(a,b,c,d):
-            return abs(c-a) + abs(d-b)
         helper = []
         for item in points:
             ite_0 = item[0]
@@ -11,20 +9,24 @@ class Solution:
                 case  = abs(ite_0-x) + abs(ite_1-y)
             else:
                 case = float(inf)
-            # store = helper(x,y,ite_0,ite_1)
+          
             helper.append(case)
-        print(helper)
-#         for item in points:
-#             if item[0]==x or item[1]==y :
-#                 store = helper(x,y,item[0],item[1])
-#                 out = min(out,store)
-#                 # store[]
-#                 print(out, item[0], item[1])
-#         print(helper)
         out = min(helper)
         if out < float(inf):
             return helper.index(out)
         else:
             return -1
         
+        
+        
+        # def helper(a,b,c,d):
+        #     return abs(c-a) + abs(d-b)
+        # for item in points:
+        #             if item[0]==x or item[1]==y :
+        #                 store = helper(x,y,item[0],item[1])
+        #                 out = min(out,store)
+        #                 store = helper(x,y,ite_0,ite_1)
+        #                 print(out, item[0], item[1])
+        #         print(helper)
+
         
