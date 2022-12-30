b3ee451cace1de47b3e42a84492d0a6f59913768
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2022-12-30 18:00:35 +0300

    Time: 2115 ms (75.77%), Space: 54 MB (19.15%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
@@ -10,13 +10,30 @@ class Solution:
             for j in range(len(cols)):
                 rows[i]+= grid[i][j]
                 cols[j]+= grid[i][j]
-        
+        print(rows)
+        print(cols)
         out = []
-        for i in range(len_row):
-                eachr = []
-                for j in range(len_cols):
-                    eachr.append(rows[i] + cols[j]- (len_cols - rows[i]) - (len_row -  cols[j]))
-                out.append(eachr)
-        return(out)
+        rowe = []
+        cole = []
+        for i  in rows:
+            case = [i, len_cols - i]
+            rowe.append(case)
+        for i  in cols:
+            case = [i, len_row - i]
+            cole.append(case)
+        print(rowe)
+        print(cole)
+        for i in range(len(rowe)):
+            case = []
+            for j in range(len(cole)):
+                case.append(rowe[i][0] + cole[j][0] - rowe[i][1] -  cole[j][1])
+            out.append(case)
+        return out
+#         for i in range(len_row):
+#                 eachr = []
+#                 for j in range(len_cols):
+#                     eachr.append(rows[i] + cols[j]- (len_cols - rows[i]) - (len_row -  cols[j]))
+#                 out.append(eachr)
+#         return(out)
                     
                                    
