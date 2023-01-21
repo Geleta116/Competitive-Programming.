ea21f1c1614fca275fe4b76c47e14e6135c44c67
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-01-22 00:09:39 +0300

    Time: 77 ms (98.76%), Space: 13.8 MB (70.35%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -1,15 +1,24 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        # minus = [-1 for i in range(len(matrix[0]+1))]
-        # matrix.insert(0,minus)
-        # for item in matrix:
-        #     item.insert(0,-1)
-        # minus = [-1 for i in range(len(matrix[0]))]
-        # matrix.insert(0,minus)
-        
         
         for row in range(1,len(matrix)):
             for col in range(1, len(matrix[0])):
                 if matrix[row][col] != matrix[row-1][col-1] :
                     return False
         return True
+    
+    
+    ############################################################# Another solution
+#         minus = [-1 for i in range(len(matrix[0]+1))]
+#         matrix.insert(0,minus)
+#         for item in matrix:
+#             item.insert(0,-1)
+#         minus = [-1 for i in range(len(matrix[0]))]
+#         matrix.insert(0,minus)
+        
+        
+#         for row in range(1,len(matrix)):
+#             for col in range(1, len(matrix[0])):
+#                 if matrix[row][col] != matrix[row-1][col-1] and  matrix[row-1][col-1] != -1:
+#                     return False
+#         return True
