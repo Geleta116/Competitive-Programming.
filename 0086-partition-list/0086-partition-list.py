ef1f58685a410e57c784084fe2db7f159708d5c0
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-02-07 12:46:41 +0300

    Time: 35 ms (78.86%), Space: 13.8 MB (71.25%) - LeetHub

--- 0086-partition-list/0086-partition-list.py
+++ 0086-partition-list/0086-partition-list.py
@@ -22,23 +22,3 @@ class Solution:
         great.next = None
         less.next = dummy2.next
         return dummy.next
-#         dummy = dummy.next
-#         dummy2 = dummy2.next
-#         less.next = dummy2
-#         return less
-        
-#         def partition(self, head, x):
-#     h1 = l1 = ListNode(0)
-#     h2 = l2 = ListNode(0)
-#     while head:
-#         if head.val < x:
-#             l1.next = head
-#             l1 = l1.next
-#         else:
-#             l2.next = head
-#             l2 = l2.next
-#         head = head.next
-#     l2.next = None
-#     l1.next = h2.next
-#     return h1.next
-        
