40a46e5ab36b611e43fc4d1642336d38993494df
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-02-24 11:43:08 +0300

    Time: 52 ms (68.72%), Space: 14.3 MB (9.91%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,19 +1,18 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        out = [-1]*len(nums1)
+        checker = defaultdict(lambda: -1)
         stack = []
-        for indi,item in enumerate(nums1):
-            ind_item = nums2.index(item)
-            l2 = nums2[ind_item::]
-            len_l2 = len(l2)
-            a = 0
-            print(l2)
-            for i in l2:
-                if i>item:
-                    out[indi]=i
-                    break
-                
-                
-        return out
+        for num in nums2:
+            if not stack:
+                stack.append(num)
+            else:
+                while stack and stack[-1]<num:
+                    checker[stack.pop()] = num
+                stack.append(num)
+        
+        result = []
+        for num in nums1:
+            result.append(checker[num])
+        return result
             
         
