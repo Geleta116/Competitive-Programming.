40f396c9028e67be2584d59b746a1ff8d21ae371
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-02-24 15:54:41 +0300

    Time: 75 ms (47.94%), Space: 14.5 MB (81.89%) - LeetHub

--- 0622-design-circular-queue/0622-design-circular-queue.py
+++ 0622-design-circular-queue/0622-design-circular-queue.py
@@ -1,77 +1,52 @@
-class ListNode:
-    
-    def __init__(self,val=0, next = None):
-        self.val = val
-        self.next = next
-        
-
 class MyCircularQueue:
 
     def __init__(self, k: int):
-        new = ListNode()
-        self.head = new
-        self.tail = new
-        self.size = 0
+        self.queue = deque()
         self.k = k
-
+        
               
 
     def enQueue(self, value: int) -> bool:
-        if self.size == 0:
-            newnode = ListNode(value)
-            self.head = newnode
-            self.tail = newnode
-            
-            self.size += 1
+        if self.k != 0:
+            self.queue.append(value)
+            self.k -= 1
             return True
-        elif self.size < self.k:
-            newnode = ListNode(value)
-            self.tail.next = newnode
-            self.tail = newnode
-            self.size += 1
-            return True
-        else:
-            return False
-        
-        
+        return False
 
+        
     def deQueue(self) -> bool:
-        if self.size>0:
-            temp = self.head 
-            self.head = self.head.next
-            temp.next = None
-            self.size -=1
-            return True
-        else:
-            return False
-        
-        
-
-    def Front(self) -> int:
-        if self.size > 0 and self.head:
-            return self.head.val
-        else:
-            return -1
-        
-        
-    def Rear(self) -> int:
-        if self.size > 0 and self.tail:
-            return self.tail.val
-        else:
-            return -1
-       
-
-    def isEmpty(self) -> bool:
-        if self.size == 0:
+        if len(self.queue)>0:
+            self.queue.popleft()
+            self.k += 1
             return True
         return False
         
+    
+    
+    def Front(self) -> int:
+        if len(self.queue) == 0:
+            return -1
+        return self.queue[0]
+        
+        
+
+    def Rear(self) -> int:
+        if len(self.queue) == 0:
+            return -1
+        return self.queue[-1]
+
+        
+    def isEmpty(self) -> bool:
+        if len(self.queue) == 0:
+            return True
+        return False
+
+        
 
     def isFull(self) -> bool:
-        if self.size == self.k:
+        if self.k == 0:
             return True
-        else:
-            return False
+        return False
         
         
 
@@ -87,14 +62,9 @@ class MyCircularQueue:
 
 
 
-
-
-
-#this is the circular linked list implementation
-
 # class ListNode:
     
-#     def __init__(self,val=None, next = None):
+#     def __init__(self,val=0, next = None):
 #         self.val = val
 #         self.next = next
         
@@ -102,59 +72,71 @@ class MyCircularQueue:
 # class MyCircularQueue:
 
 #     def __init__(self, k: int):
-#         self.head = ListNode()
-#         self.tail = self.head
-#         while k > 0:
-#             newnode = ListNode()
-#             self.tail.next = newnode
-#             self.tail = newnode
-#             k -= 1
-#         self.tail.next = self.head
-#         self.tail = self.head
-        
+#         new = ListNode()
+#         self.head = new
+#         self.tail = new
+#         self.size = 0
+#         self.k = k
+
               
 
 #     def enQueue(self, value: int) -> bool:
-#         if self.tail == None:
-#             self.tail.val = value
-#             self.tail = self.tail.next
+#         if self.size == 0:
+#             newnode = ListNode(value)
+#             self.head = newnode
+#             self.tail = newnode
+            
+#             self.size += 1
 #             return True
-#         return False`
+#         elif self.size < self.k:
+#             newnode = ListNode(value)
+#             self.tail.next = newnode
+#             self.tail = newnode
+#             self.size += 1
+#             return True
+#         else:
+#             return False
         
         
 
 #     def deQueue(self) -> bool:
-#         if self.head == None:
-#             return False
-#         else:
-#             self.head.val = None
+#         if self.size>0:
+#             temp = self.head 
 #             self.head = self.head.next
+#             temp.next = None
+#             self.size -=1
 #             return True
-#         return False
+#         else:
+#             return False
+        
         
 
 #     def Front(self) -> int:
-#         if self.head:
+#         if self.size > 0 and self.head:
 #             return self.head.val
-#         return -1
+#         else:
+#             return -1
         
-
+        
 #     def Rear(self) -> int:
-#         if self.tail:
+#         if self.size > 0 and self.tail:
 #             return self.tail.val
-#         return -1
-        
+#         else:
+#             return -1
+       
 
 #     def isEmpty(self) -> bool:
-#         if self.head:
-#             return False
-#         return True
+#         if self.size == 0:
+#             return True
+#         return False
         
 
 #     def isFull(self) -> bool:
-#         if self.tail.next != None:
+#         if self.size == self.k:
 #             return True
-#         return False
+#         else:
+#             return False
+        
         
 
 
@@ -166,3 +148,11 @@ class MyCircularQueue:
 # # param_4 = obj.Rear()
 # # param_5 = obj.isEmpty()
 # # param_6 = obj.isFull()
+
+
+
+
+
+
+#this is the circular linked list implementation
+
