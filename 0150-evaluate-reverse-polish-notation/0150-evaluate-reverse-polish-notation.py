bf9193e059d5596641941510ecbb79d70842baef
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-02-24 23:13:21 +0300

    Time: 65 ms (85.86%), Space: 14.4 MB (49.15%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -2,23 +2,23 @@ class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         new_stack = []
         op = ["+","-","*","/"]
-        for i in range(len(tokens)):
-            if tokens[i] not in op:
-                new_stack.append(tokens[i])
+        for token in tokens:
+            if token not in op:
+                new_stack.append(token)
             else:
-                if tokens[i] == "+":
+                if token == "+":
                     token1 = int(new_stack.pop())
                     token2 = int(new_stack.pop())
                     new_stack.append(token1 + token2)
-                elif tokens[i] == "-":
+                elif token == "-":
                     token1 = int(new_stack.pop())
                     token2 = int(new_stack.pop())
                     new_stack.append(token2 - token1)
-                elif tokens[i] == "*":
+                elif token == "*":
                     token1 = int(new_stack.pop())
                     token2 = int(new_stack.pop())
                     new_stack.append(token1 * token2)
-                elif tokens[i] == "/":
+                elif token == "/":
                     token1 = int(new_stack.pop())
                     token2 = int(new_stack.pop())
                     new_stack.append(int(token2 / token1))
