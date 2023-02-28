f8b9225d359f76f8fc631fc66dacfa3a25234f98
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-02-28 17:57:15 +0300

    Time: 36 ms (42.58%), Space: 13.8 MB (56.15%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -4,11 +4,13 @@ class Solution:
             return 1 
         elif n == 1:
             return x
-        elif n > 0 and n%5 == 0:
+        elif n > 0 :
+            if n%5 == 0:
                 n = n//5
                 temp = self.myPow(x,n)
                 return temp*temp*temp*temp*temp
-        elif n > 0:
+            else:
+        
                 n -= 1
                 return x*self.myPow(x,n)
         else:
