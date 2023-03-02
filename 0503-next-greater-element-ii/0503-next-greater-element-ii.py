077b2a7e07e40d3c6e22e781fb75f75d86463e7f
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-03-02 11:05:31 +0300

    Time: 234 ms (49.47%), Space: 17.2 MB (6.47%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -1,14 +1,43 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        out = [-1]*len(nums)
-        s = []
-        lnums = nums*2
-        for i in range(len(lnums)):
-            while s and s[-1][0]<lnums[i]:
-                val,pos = s.pop()
-                out[pos] = lnums[i]
-            if i<len(nums):
-                s.append(( lnums[i],i))
-        return out
+        
+        #initialize an array with -1
+        next_greater_list = [-1 for index in range(len(nums))]
+        
+        #create a dictionary to store indices and values
+        #create a monotonic stack
+        stack = []
+        container = defaultdict(lambda : 0)
+        
+        #get the original length of the array
+        #concatinate the array for circular purpose
+        #exclude the last element
+        len_nums = len(nums)
+        nums = nums + list(nums[:-1])
+        
+        
+        #iterate and fill the stack appropriately
+        for index,num in enumerate(nums):
+            
+            if not stack:
+                stack.append([num , index])
+            else:
+                if stack[-1][0] >= num:
+                    stack.append([num , index])
+                else:
+                    while stack and stack[-1][0] < num:
+                        integer, ind = stack.pop()
+                        container[ind] = num
+                    stack.append([num , index])
+        
+        #populate the output array with the new founf index and numbers
+        for keys in container:
+            if keys < len_nums :
+                next_greater_list[keys] = container[keys]
+                
+        #return the output
+        return next_greater_list
+                
+        
             
        
