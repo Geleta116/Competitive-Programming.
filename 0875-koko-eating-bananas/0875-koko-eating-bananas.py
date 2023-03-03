bd12bd963b58fdf50880876ad95ef97b23fc7092
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-03-03 16:29:23 +0300

    Time: 383 ms (95.95%), Space: 15.5 MB (64.11%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        #This is the initialization of the left and the right pointers
+        left = 0
+        right = max(piles)
+        
+        
+        #This is the validate function
+        def validate(piles,speed):
+            total_hour = 0
+            for banana in piles:
+                hour =  banana / speed
+                total_hour += ceil(hour)
+            if total_hour <= h:
+                return True
+            return False
+        
+        
+        #This is the binary search implementation
+        while right > left + 1:
+            mid = left + (right - left)//2
+            if validate(piles,mid):
+                right = mid
+            else:
+                left = mid
+        return right
+    
+        
+        
