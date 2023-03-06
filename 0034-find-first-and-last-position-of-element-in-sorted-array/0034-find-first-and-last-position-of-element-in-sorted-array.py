40cb007de7a24664ac13c0aca8a0b1c0b6d1203b
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-03-06 17:23:47 +0300

    Time: 85 ms (80.52%), Space: 15.5 MB (39.15%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,10 +1,44 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        first = bisect_left(nums,target)
-        last = bisect_right(nums,target)
-        print(first,last)
-        if first == len(nums)  or len(nums) == 0 or nums[first] != target or (((last - 1) > -1) and nums[last - 1] != target):
+        left = -1
+        right = len(nums)
+        out = []
+        if len(nums) == 0:
             return [-1, -1]
+        def validate(n):
+            if nums[n] >= target:
+                return True
+            return False
+        
+        def validate2(n):
+            if nums[n] <= target:
+                return True
+            return False
+            
+        while right >  left + 1:
+            mid = left + (right - left) // 2
+            if validate(mid):
+                right = mid
+            else:
+                left = mid
+        
+        right1 = right
+        left = -1
+        right = len(nums)
+        
+        while right >  left + 1:
+            mid = left + (right - left) // 2
+            if validate2(mid):
+                left = mid
+            else:
+                right = mid
+        
+        if right1 < len(nums) and left < len(nums) and nums[right1] ==  target and nums[left] == target:
+            return [right1, left]
         else:
-            return[first,last - 1]
+            return [-1,-1]
+        
+        
+        
+        
         
