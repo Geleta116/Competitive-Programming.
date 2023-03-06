6fa284d4f935ab6608cb061e0b63cf9f464b2585
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-03-06 15:24:53 +0300

    Update 0907-sum-of-subarray-minimums.py

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
@@ -1,16 +1,34 @@
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
+        """
+        IDEA BEHIND THE CODE:- 
+        We will create a monotonic increasing stack 
+        and when we encounter a smaller value than the top of the stack we will pop the stack
+        and count for how manu numbers the popped value was minimum for and we do this by multiplication
+        
+        pre will store the difference of the index of the first lower value
+        post will store the difference between the index of the current and the popped value
+        then we will multiply pre and post and then multiply it by the value of the popped value
+        and then we will store it.
+        
+        """
         stack = []
+        #we appended a zero to remove all elements if the stack isnt empty at the end
         arr.append(0)
         minsum = 0
+        
+        #We will iterate over the arr and fill the stack appropriately
         for index,num in enumerate(arr):
-            
+            #if the stack is empty just add the value and the index
             if not stack:
                 stack.append([num,index])
             else:
+                #if the end of the stack is smaller than the current value just append to the stack
                 if stack[-1][0] <= num:
                     stack.append([num,index])
                 else:
+                    # if the current value is smaller than the stack head we will pop it and calculate the
+                    #number of pre and post subarray minimums
                     while stack and stack[-1][0] > num:
                         value,ind = stack.pop()
                         pre = index - ind
@@ -21,16 +39,8 @@ class Solution:
                         minsum += pre * post * value
                     stack.append([num,index])
             
-             
+            
         return minsum % (10**9 + 7)
-                    
-                    
-                
-                
-        
-        
-        
-        
         
         #don't forget to return in modulo 10**9 + 7
         
