d88cc421c543bcd43b8ec8cb7a3296ae6144038a
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-03-07 14:56:40 +0300

    Time: 135 ms (74.98%), Space: 17 MB (42.19%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.py
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.py
@@ -6,26 +6,54 @@
 #         self.right = right
 class Solution:
     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        """
+        RECURSIVE METHOD
+        """
         insertnode = TreeNode(val)
-        temp = root
         if not root:
-            temp = insertnode
-        else:
-            while root:
-                if  root.val < val:
-                    if root.right:
-                        root = root.right
-                    else:
-                        root.right = insertnode
-                        break
-                elif root.val >= val:
-                    if root.left:
-                        root = root.left
-                    else:
-                        root.left = insertnode
-                        break
+            return insertnode
+        elif root.val < val:
+            root.right = self.insertIntoBST(root.right,val)
+            return root
+        elif root.val >= val:
+            root.left = self.insertIntoBST(root.left,val)
+            return root
         
-        return temp
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        """
+        ITERATIVE METHOD
+        """
+#         insertnode = TreeNode(val)
+#         temp = root
+#         if not root:
+#             temp = insertnode
+#         else:
+#             while root:
+#                 if  root.val < val:
+#                     if root.right:
+#                         root = root.right
+#                     else:
+#                         root.right = insertnode
+#                         break
+#                 elif root.val >= val:
+#                     if root.left:
+#                         root = root.left
+#                     else:
+#                         root.left = insertnode
+#                         break
+        
+#         return temp
         
             
         
