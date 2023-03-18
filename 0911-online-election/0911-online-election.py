d4650ff1db6f2fa71b3195a705f7f5c70260aa59
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-03-18 18:49:03 +0300

    Time: 964 ms (16.56%), Space: 19.7 MB (83.22%) - LeetHub

--- 0911-online-election/0911-online-election.py
+++ 0911-online-election/0911-online-election.py
@@ -1,56 +1,52 @@
 class TopVotedCandidate:
-    def __init__(self, persons, times):
-        self.leads, self.times, count = [], times, {}
-        lead = -1
-        for p in persons:
-            count[p] = count.get(p, 0) + 1
-            if count[p] >= count.get(lead, 0): lead = p
-            self.leads.append(lead)
+   
 
-    def q(self, t):
-        return self.leads[bisect.bisect(self.times, t) - 1]
-
-#     def __init__(self, persons: List[int], times: List[int]):
-#         self.persons = persons
-#         self.times = times
+    def __init__(self, persons: List[int], times: List[int]):
+        self.persons = persons
+        self.times = times
+        self.leader = self.persons[0]
+        self.values = defaultdict(int)
+        self.values[self.leader] += 1
+        for item in range(1,len(self.persons)):
+            self.values[self.persons[item]] += 1
+            if self.values[self.leader] > self.values[self.persons[item]]:
+                self.persons[item] = self.leader
+                
+                
+            else:
+                self.leader =  self.persons[item]
+            
         
-#         self.leader = self.persons[0]
-#         self.values = defaultdict(int)
-#         self.values[self.leader] += 1
-#         for item in range(1,len(self.persons)):
-#             self.values[self.persons[item]] += 1
-#             if self.leader > self.values[self.persons[item]]:
-#                 self.persons[item] = self.leader
         
             
    
 
-#     def q(self, t: int) -> int:
-#         def validate(mid,t):
-#             if self.times[mid] >= t:
-#                 return True
-#             return False
+    def q(self, t: int) -> int:
+        def validate(mid,t):
+            if self.times[mid] >= t:
+                return True
+            return False
             
         
 
-#         start = -1
-#         end = len(self.persons)
-#         while end > start + 1:
-#             mid = start + (end - start ) // 2
-#             if validate(mid,t):
-#                 end = mid
-#             else:
-#                 start = mid
-#         if end >= len(self.times):
-#             return self.persons[-1]
-#         else:
-#             if self.times[end] == t:
-#                 return self.persons[end]
-#             else:
-#                 if end == 0:
-#                     return self.persons[0]
-#                 else:
-#                     return self.persons[end - 1]
+        start = -1
+        end = len(self.persons)
+        while end > start + 1:
+            mid = start + (end - start ) // 2
+            if validate(mid,t):
+                end = mid
+            else:
+                start = mid
+        if end >= len(self.times):
+            return self.persons[-1]
+        else:
+            if self.times[end] == t:
+                return self.persons[end]
+            else:
+                if end == 0:
+                    return self.persons[0]
+                else:
+                    return self.persons[end - 1]
         
        
 # #         if end >= len(self.times):
