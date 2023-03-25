ff63ff9973b42df759f3bbe81985968b8a96cbf9
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-03-25 22:23:46 +0300

    Time: 285 ms (5.08%), Space: 17 MB (25.42%) - LeetHub

---
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -0,0 +1,43 @@
+class Solution:
+    def getHappyString(self, n: int, k: int) -> str:
+        letters = ["a", "b", "c"]
+        self.container = []
+        self.out = ""
+        
+        
+        def backtrack(arr):
+            current = "".join(arr)
+            
+            if len(arr) > n:
+                return
+            
+            if n>=2 and len(arr) == n and arr[-2] != arr[-1]:
+                current = "".join(arr)
+                
+                self.container.append(current)
+                return
+            if n ==  1 and len(arr) == n:
+                current = "".join(arr)
+                
+                self.container.append(current)
+                return
+                
+            if len(arr)>=2 and arr[-2] == arr[-1]:
+                return
+            
+            for index in range(len(letters)):
+                arr.append(letters[index])
+                backtrack(arr)
+                arr.pop()
+        
+        
+        for index in range(len(letters)):
+            arr = []
+            arr.append(letters[index])
+            backtrack(arr)
+        self.container.sort()
+        if k <= len(self.container):
+                return self.container[k - 1]
+        return self.out
+            
+            
