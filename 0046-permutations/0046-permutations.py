e37111f7bac90669ac0eeb9ebd160fb0b77c957a
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-03-25 20:35:42 +0300

    Time: 47 ms (28.47%), Space: 14.1 MB (49.98%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,25 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        self.permutations = []
+        def backtrack(arr):
+            if len(set(arr)) ==  len(nums):
+                self.permutations.append(arr[:])
+                return
+            elif len(set(arr)) != len(arr):
+                return
+            
+            for index in range(len(nums)):
+                arr.append(nums[index])
+                backtrack(arr)
+                arr.pop()
+        
+        
+        for index in range(len(nums)):
+            arr = []
+            arr.append(nums[index])
+            backtrack(arr)
+        
+       
+        return self.permutations
+        
+         
