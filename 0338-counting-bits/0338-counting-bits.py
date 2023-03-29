906b5135eec22f2c8bb9aa8451669852529d7a60
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-03-29 18:38:23 +0300

    Time: 98 ms (42.07%), Space: 20.6 MB (97.14%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,16 +1,20 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
         output = []
+        previous = defaultdict(int)
         for numbers in range(n + 1):
             count = 0
-            
+            first = numbers
             while numbers:
                 temp = numbers
                 numbers = numbers & 1
                 if numbers != 0 :
                     count += 1
                 numbers = temp >> 1
-                
+                if numbers in previous:
+                    count += previous[numbers]
+                    break
+            previous[first] = count
             output.append(count)
         return output
                 
