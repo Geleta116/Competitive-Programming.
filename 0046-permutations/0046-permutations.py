77c3b4ae262cfc9d94a5170986933b88e0442ac4
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-03-31 22:18:47 +0300

    Time: 46 ms (31.02%), Space: 14 MB (49.64%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,21 +1,26 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         self.permutations = []
-        case = set()
+        self.case = 0
         def backtrack(arr):
-            if len(case) ==  len(nums):
+            
+            if 2 ** len(nums)  - 1 == self.case: 
                 self.permutations.append(arr[:])
                 return
             
             
             for index in range(len(nums)):
-                if nums[index] in case:
+                if 2 ** index & self.case:
                     continue
                 arr.append(nums[index])
-                case.add(nums[index])
+                temp = self.case
+                self.case =  (1 << index) | self.case
+                
                 backtrack(arr)
                 arr.pop()
-                case.remove(nums[index])
+                self.case = temp
+                
+                
         
         
         backtrack([])
