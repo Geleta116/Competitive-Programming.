bc794e02a3ab3bd5756e4508da66c8ce0edb9669
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-04-04 18:06:18 +0300

    Time: 578 ms (80.88%), Space: 17.3 MB (75.74%) - LeetHub

--- 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
@@ -1,13 +1,12 @@
 class Solution:
     def maxProduct(self, words: List[str]) -> int:
         maximum = 0
-        alphabet = {"a":1,"b":2,"c":4, "d":8, "e":16, "f": 2**5, "g":2**6, "h":2**7,"i":2**8,"j":2**9,"k":2**10,"l":2**11,"m":2**12, "n":2**13,"o":2**14, "p":2**15,"q":2**16,"r":2**17,"s":2**18,"t":2**19,"u":2**20,"v":2**21,"w":2**22,"x":2**23,"y":2**24,"z":2**25}
-        
+       
         case = []
         for word in words:
             current = 0
             for letter in word:
-                current |= alphabet[letter]
+                current |= 1 << ord(letter) - 96
             case.append(current)
         output = 0
         for i in range(len(words)):
