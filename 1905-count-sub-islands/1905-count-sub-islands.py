b00b511a79235f814c14f4c66ff9caf5a2cda2a2
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-04-14 17:34:16 +0300

    Time: 3419 ms (26.15%), Space: 94.3 MB (37.26%) - LeetHub

---
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -0,0 +1,58 @@
+class Solution:
+    def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
+        directions = [(0,1),(0,-1),(1,0),(-1,0)]
+        
+        def inbound(row, col):
+            return 0<= row < len(grid2) and 0<= col < len(grid2[0]) and grid2[row][col] == 1
+        
+        self.visited = [[False for i in range(len(grid1[0]))] for j in range(len(grid1))]
+        self.count = 0
+        #         if grid2[row][col] == 0:
+        #                 return 
+
+        #             if self.visited[row][col]:
+        #                 return
+            
+        def dfs(row, col):
+            self.visited[row][col] = True
+            
+            if grid2[row][col] and grid2[row][col] != grid1[row][col]:
+                self.status  =  False
+            
+            if not grid2[row][col]:
+                return 
+                
+            
+            for direction in directions:
+                    
+                    addedRow = direction[0]
+                    addedCol = direction[1]
+                    newRow = row + addedRow
+                    newCol = col + addedCol
+                    
+                    if inbound(newRow, newCol) and not self.visited[newRow][newCol]:
+                        if grid2[row][col] and grid2[newRow][newCol] != grid1[newRow][newCol]:
+                            self.status  =  False
+                        dfs(newRow, newCol)
+            if self.status:
+                return True
+                        
+            
+        
+        for row in range(len(grid2)):
+            for col in range(len(grid2[0])):
+                if not self.visited[row][col]:
+                    self.status = True
+                    store = dfs(row, col)
+                    if store:
+                        # print(row,col)
+                        self.count += 1
+        
+        return self.count
+                    
+            
+        
+        
+        
+        
+        
