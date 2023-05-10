313fa9fff447dab751721377eb62f2245efe5814
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-05-10 17:46:01 +0300

    Time: 118 ms (17.67%), Space: 17.8 MB (21.31%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,31 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        indegree = [ 0 for _ in range(numCourses)]
+        
+        graph = defaultdict(list)
+        
+        for prereq in prerequisites:
+            graph[prereq[1]].append(prereq[0])
+            indegree[prereq[0]] += 1
+        
+        queue = deque()
+        
+        for index in range(len(indegree)):
+            if indegree[index] == 0:
+                queue.append(index)
+        
+        output = []
+
+        while queue:
+            current = queue.pop()
+            if indegree[current] == 0:
+                output.append(indegree)
+            
+            for child in graph[current]:
+                indegree[child] -= 1
+                if indegree[child] == 0:
+                    queue.append(child)
+        if len(output) == len(indegree):
+            return True
+        return False
+        
