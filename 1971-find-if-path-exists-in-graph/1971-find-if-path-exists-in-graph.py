ccc5c1dc0ea5654100b946e493ebc1f0eec57f2c
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-05-17 18:22:54 +0300

    Time: 3480 ms (5.01%), Space: 108.9 MB (50.55%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,24 +1,110 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        def dfs(node):
-            if node == destination:
-                return True
-            
+        
+        rep = [i for i in range(n)]
+        size = [1  for _ in range(n)]
+        graph = defaultdict(list)
+        
+        def bfs(node, val):
+            queue = deque()
+            queue.append(node)
+            visited = set()
             visited.add(node)
             
-            for neighbour in self.graph[node]:
-                if neighbour not in visited:
-                    found = dfs(neighbour)
-                    if found:
-                        return True
-            return False
-                    
+            while queue:
+                curr = queue.popleft()
+                rep[curr] = val
+                
+                for child in graph[curr]:
+                    if child not in visited:
+                        visited.add(child)
+                        queue.append(child)
+                
             
-        visited = set()
-        self.graph = defaultdict(list)
+            
+            
+            
+           
         for edge in edges:
-            self.graph[edge[0]].append(edge[1])
-            self.graph[edge[1]].append(edge[0])
-        return dfs(source)
+           
+            if rep[edge[0]] != rep[edge[1]]:
+                
+                if size[rep[edge[0]]] >= size[rep[edge[1]]]:
+                    
+                    size[rep[edge[0]]] += size[rep[edge[1]]]
+                    store = rep[edge[0]]
+                    bfs(edge[1], store)
+                    graph[edge[0]].append(edge[1])
+                    graph[edge[1]].append(edge[0])
+                    
+                    
+                    
+                else:
+                    
+                    size[rep[edge[1]]] += size[rep[edge[0]]]
+                    store = rep[edge[1]]
+                    bfs(edge[0], store)
+                    graph[edge[1]].append(edge[0])
+                    graph[edge[0]].append(edge[1])
+                    
         
         
+        return rep[source] == rep[destination]
+                    
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         rep = [i for i in range(n)]
+        
+#         for edge in edges:
+            
+#             if rep[edge[0]] != rep[edge[1]]:
+#                 x = rep[edge[0]]
+#                 while x != rep[x]:
+#                     x = rep[x]
+                    
+#                 y = rep[edge[1]]
+#                 while y != rep[y]:
+#                     y = rep[y]
+#                     rep[y] = x
+                    
+#                 rep[edge[1]] = x
+                
+#         return edge
+                
