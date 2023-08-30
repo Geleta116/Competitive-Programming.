010d3722f265852e55583ee42dd2d144727edd18
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-08-30 21:28:53 +0300

    Time: 592 ms (43.78%), Space: 65.3 MB (5.00%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -0,0 +1,39 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        par  = defaultdict(int)
+        rank = defaultdict(int)
+        for num in nums:
+            par[num] = num
+            rank[num] = 1
+        
+        def find(x):
+            if x != par[x]:   
+                par[x] =  find(par[x])
+            return par[x]
+        
+        def union(num1, num2):
+            par1, par2 = find(num1), find(num2)
+            if par1 >= par2:
+                par[par2] = par1
+                find(num2)
+            
+            else:
+                par[par1] = par2
+                find(num1)
+        
+        for num in nums:
+            if (num + 1 ) in par:
+                union(num, num + 1)
+        for num in nums:
+            find(num)
+        count = 1
+        counter = defaultdict(int)
+        for key in par:
+            counter[par[key]] += 1
+        
+        if counter.values():
+            return max(counter.values())
+        return 0
+       
+                
+                
