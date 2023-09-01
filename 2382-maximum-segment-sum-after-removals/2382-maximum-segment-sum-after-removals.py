530d8904c5661bd1023e9b0e55b2754a19f16363
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-09-01 07:05:19 +0300

    Time: 1164 ms (70.30%), Space: 50.6 MB (39.60%) - LeetHub

--- 2382-maximum-segment-sum-after-removals/2382-maximum-segment-sum-after-removals.py
+++ 2382-maximum-segment-sum-after-removals/2382-maximum-segment-sum-after-removals.py
@@ -1,14 +1,14 @@
 class Solution:
     def maximumSegmentSum(self, nums: List[int], removeQueries: List[int]) -> List[int]:
         n = len(nums)
-        parent = [i for i in range(n)]  # Initially each element is its own parent
-        rank = [0] * n                  # Rank array for optimization
-        sum_values = [nums[i] for i in range(n)]  # Initialize sum values with the element values
+        parent = [i for i in range(n)]  
+                        
+        sum_values = [nums[i] for i in range(n)] 
         visited = set()
-        # Find operation
+       
         def find(x):
             if parent[x] != x:
-                parent[x] = find(parent[x])  # Path compression
+                parent[x] = find(parent[x])  
             return parent[x]
 
         
@@ -19,26 +19,26 @@ class Solution:
             if root_x == root_y:
                 return
 
-            if rank[root_x] > rank[root_y]:
-                parent[root_y] = root_x
-                sum_values[root_x] += sum_values[root_y]
-            else:
-                parent[root_x] = root_y
-                sum_values[root_y] += sum_values[root_x]
-                if rank[root_x] == rank[root_y]:
-                    rank[root_y] += 1
+            parent[root_x] = root_y
+            sum_values[root_y] += sum_values[root_x]
+                
+                    
         output = [0]
         for index in range(len(nums) - 1, -1, -1):
            
             visited.add(removeQueries[index])
             if removeQueries[index] > 0 and  removeQueries[index] - 1 in visited:
                 union(removeQueries[index], removeQueries[index] - 1 )
+                
             if removeQueries[index] < (len(nums) - 1) and  removeQueries[index] + 1 in visited:
                 union(removeQueries[index], removeQueries[index] + 1 ) 
+                
             curr = sum_values[find(removeQueries[index])]
             maxi = max(output[-1], curr)
             output.append(maxi)
+            
         if output:
             output.pop()
+            
         return output[::-1]
             
