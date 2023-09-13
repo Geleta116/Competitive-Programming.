56545ee7c61644ae1530e53b41fe4d8093633a2b
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-09-13 22:15:59 +0300

    Time: 600 ms (83.66%), Space: 16.8 MB (77.78%) - LeetHub

---
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.py
@@ -0,0 +1,44 @@
+class Solution:
+    def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+        
+        # data structures
+        self.graph = defaultdict(list)
+        self.visited = set()
+        self.queue = deque()
+        compCount = 0
+        
+        # building the graph
+        for edge in edges:
+            self.graph[edge[0]].append(edge[1])
+            self.graph[edge[1]].append(edge[0])
+        
+        
+        #bfs implementation
+        def bfs(node):
+            self.queue.append(node)
+            self.visited.add(node)
+            nodeCount = 0
+            edgeCount = 0
+            while self.queue:
+                nodeCount += 1
+                curr  = self.queue.popleft()
+                for child in self.graph[curr]:
+                    edgeCount += 1
+                    if child not in self.visited:
+                        
+                        self.queue.append(child)
+                        self.visited.add(child)
+            if edgeCount == 0:
+                return True
+            
+            return nodeCount * (nodeCount - 1) == 2 * edgeCount // 2
+
+            
+            
+        for vertice in range(n):
+            if vertice not in self.visited:
+                isComplete = bfs(vertice)
+                if isComplete:
+                    compCount += 1
+                
+        return compCount
