fefaafedca2e6b6d2d5485c2404a7100b5b10e6b
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-10-03 17:20:56 +0300

    Time: 108 ms (50.92%), Space: 17.1 MB (38.69%) - LeetHub

---
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -0,0 +1,33 @@
+import heapq
+
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        def inbound(row, col):
+            return 0 <= row < len(grid) and 0 <= col < len(grid[0])
+    
+        heap = [(grid[0][0], 0, 0, 0)]
+        heapq.heapify(heap)
+
+        visited = set()
+
+        while heap:
+
+            currCost, currDiff, r, c = heapq.heappop(heap)
+
+            if r == len(grid) - 1 and c == len(grid[0]) - 1:
+                return currCost
+
+            for addedRow, addedCol in directions:
+                newRow =  r + addedRow
+                newCol = c + addedCol 
+
+                if inbound(newRow, newCol) and  (newRow, newCol) not in visited:
+                    visited.add((newRow, newCol))
+                    if grid[newRow][newCol] <= currCost:
+
+                        heapq.heappush(heap, (currCost, currDiff, newRow, newCol))
+                    else:
+                        diff =  grid[newRow][newCol] - currCost
+                        heapq.heappush(heap, (currCost + diff, diff, newRow, newCol))
