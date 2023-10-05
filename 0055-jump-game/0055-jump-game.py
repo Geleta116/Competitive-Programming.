d73df455fef782b09c55ceded4448e0635f10040
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-10-05 16:38:18 +0300

    Time: 6396 ms (5.73%), Space: 46.9 MB (5.19%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,16 +1,26 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         
-        @cache
+        dpdict = defaultdict(int)
         def dp(idx):
             # print(idx)
             if idx == len(nums) - 1:
                 return True
+            if idx in dpdict:
+                return dpdict[idx]
+            
             for i in range(idx + 1, min(len(nums), nums[idx] + idx+1)):
-                # print(idx,i)
-                store = dp(i)
+                if i in dpdict:
+                    store = dpdict[i]
+                else:
+                    store = dp(i)
+                    dpdict[i] = store
                 if store:
                     return True
+            dpdict[idx] = False
             return False
             
         return dp(0)
+    
+    
+    
