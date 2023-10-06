ec210368e13a1635aedced0858bfcd27d2bb2ecc
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-10-06 23:49:57 +0300

    Time: 37 ms (75.95%), Space: 16.2 MB (84.94%) - LeetHub

--- 0343-integer-break/0343-integer-break.py
+++ 0343-integer-break/0343-integer-break.py
@@ -1,14 +1,24 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
-        tmp = n%3
-        if n == 3:
-            return 2
         if n == 2:
             return 1
-        if tmp == 1:
-            return 3**(n//3-1)*4
-        if tmp == 2:
-            return 3**(n//3)*2
-        if tmp == 0:
-            return 3**(n//3)
+        if n == 3:
+            return 2
         
+        @cache
+        def dp(n):
+            if n ==  1:
+                return 1
+            if n <= 3:
+                return n
+            
+            maxi = 0
+            
+            for i in range(1, n):
+                    
+                    store = dp(i) * dp(n - i)
+                    maxi = max(maxi, store)
+                
+            return maxi
+        
+        return dp(n)
