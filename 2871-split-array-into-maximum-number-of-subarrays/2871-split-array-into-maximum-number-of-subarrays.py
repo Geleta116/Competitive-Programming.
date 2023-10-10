6cce787d89b97a8813574ecf968c4f36e2054e0f
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-10-10 23:29:27 +0300

    Time: 864 ms (31.72%), Space: 27.5 MB (32.80%) - LeetHub

--- 2871-split-array-into-maximum-number-of-subarrays/2871-split-array-into-maximum-number-of-subarrays.py
+++ 2871-split-array-into-maximum-number-of-subarrays/2871-split-array-into-maximum-number-of-subarrays.py
@@ -1,24 +1,18 @@
 class Solution:
-    def maxSubarrays(self, nums: List[int]) -> int:
-        temp = nums[:]
-        for i in range(1, len(nums)):
-            temp[i] = temp[i - 1] and temp[i]
-        
+    def maxSubarrays(self, nums: List[int]) -> int:      
         output = 0
         temp2 = nums[:]
-        i = 0
-        j = 0
         temp2.append(0)
         
         for i in range(len(nums)):
-            
             if (temp2[i]) == 0 :
-                output += 1
-                     
+                output += 1     
             else:
                 temp2[i + 1] = temp2[i + 1]  & temp2[i]
+                
         if output == 0:
             return 1
+        
         return output 
             
         
