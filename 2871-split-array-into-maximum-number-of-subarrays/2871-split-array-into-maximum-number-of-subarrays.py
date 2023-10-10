66865db30dae39f2ffb1ffddfc729f2a20a6b271
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-10-10 23:29:49 +0300

    Time: 831 ms (65.99%), Space: 27.4 MB (32.80%) - LeetHub

--- 2871-split-array-into-maximum-number-of-subarrays/2871-split-array-into-maximum-number-of-subarrays.py
+++ 2871-split-array-into-maximum-number-of-subarrays/2871-split-array-into-maximum-number-of-subarrays.py
@@ -3,16 +3,13 @@ class Solution:
         output = 0
         temp2 = nums[:]
         temp2.append(0)
-        
         for i in range(len(nums)):
             if (temp2[i]) == 0 :
                 output += 1     
             else:
                 temp2[i + 1] = temp2[i + 1]  & temp2[i]
-                
         if output == 0:
             return 1
-        
         return output 
             
         
