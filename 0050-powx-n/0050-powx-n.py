d36b3e33a0df76b5c6ced2b25a13c75a235810ca
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-10-16 22:49:48 +0300

    Update 0050-powx-n.py

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,20 +1,16 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if n == 0:
-            return 1 
-        elif n == 1:
-            return x
-        elif n > 0 :
-            if n%5 == 0:
-                n = n//5
-                temp = self.myPow(x,n)
-                return temp*temp*temp*temp*temp
-            else:
         
-                n -= 1
-                return x*self.myPow(x,n)
+        if n == 0:
+            return 1
+        if n < 0:
+            return 1/self.myPow(x, -n)
+        if n % 2 == 0:
+            half =  self.myPow(x,n//2)
+            return half * half
+        
         else:
-            n = -n
-            n -= 1
-            return 1/x*self.myPow(1/x,n)
+            half = self.myPow(x, (n-1)//2)
+            return half * half * x
+        
         
