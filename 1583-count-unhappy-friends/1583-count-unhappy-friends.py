6cb435324f770f57cb1c83829582dba0cfad16ac
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-10-17 15:55:52 +0300

    Time: 367 ms (28.03%), Space: 29.9 MB (78.54%) - LeetHub

--- 1583-count-unhappy-friends/1583-count-unhappy-friends.py
+++ 1583-count-unhappy-friends/1583-count-unhappy-friends.py
@@ -17,25 +17,42 @@ class Solution:
             pair_dict[pair[1]] = pair[0]
         
         count = 0
-        
-        
+                
         for pair in pairs:
-            friendA, friendB = pair[0], pair[1]
-            prefA, prefB = preference[friendA], preference[friendB]
-
-            curr_pair_preference_A = matrix[friendA][friendB]
-            curr_pair_preference_B = matrix[friendB][friendA]
-
-            for i in range(curr_pair_preference_A):
-                otherFriend = prefA[i]
-                if matrix[otherFriend][friendA] < matrix[otherFriend][pair_dict[otherFriend]]:
-                    count += 1
-                    break
-
-            for i in range(curr_pair_preference_B):
-                otherFriend = prefB[i]
-                if matrix[otherFriend][friendB] < matrix[otherFriend][pair_dict[otherFriend]]:
-                    count += 1
-                    break
-
+            curr_pair_preference = matrix[pair[0]][pair[1]]
+            for i in range(curr_pair_preference):
+                if preference[pair[0]][i] in pair_dict:
+                    if matrix[preference[pair[0]][i]][pair[0]] < matrix[preference[pair[0]][i]][pair_dict[preference[pair[0]][i]]]:
+                        count += 1
+                        break
+                        
+            curr_pair_preference = matrix[pair[1]][pair[0]]
+            for i in range(curr_pair_preference):
+                if preference[pair[1]][i] in pair_dict:
+                    if matrix[preference[pair[1]][i]][pair[1]] < matrix[preference[pair[1]][i]][pair_dict[preference[pair[1]][i]]]:
+                        count += 1
+                        break
+        
         return count
+    
+        
+#         for pair in pairs:
+#             friendA, friendB = pair[0], pair[1]
+#             prefA, prefB = preference[friendA], preference[friendB]
+
+#             curr_pair_preference_A = matrix[friendA][friendB]
+#             curr_pair_preference_B = matrix[friendB][friendA]
+
+#             for i in range(curr_pair_preference_A):
+#                 otherFriend = prefA[i]
+#                 if matrix[otherFriend][friendA] < matrix[otherFriend][pair_dict[otherFriend]]:
+#                     count += 1
+#                     break
+
+#             for i in range(curr_pair_preference_B):
+#                 otherFriend = prefB[i]
+#                 if matrix[otherFriend][friendB] < matrix[otherFriend][pair_dict[otherFriend]]:
+#                     count += 1
+#                     break
+
+#         return count
