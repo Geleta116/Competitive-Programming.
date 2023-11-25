5585d59ad535c1db8b6c5de2413c46cc3322f090
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-11-25 23:08:32 +0300

    Added solution for Unique Number of Occurrences

---
+++ AfterCamp/unique-number-of-occurrences.py
@@ -0,0 +1,24 @@
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        arr.sort()
+        arr.append(1001)
+        count = set()
+        
+        left = 0
+        right = 1
+        curr = 1
+        
+        while right < len(arr):
+            if arr[right] == arr[left]:
+                curr += 1
+                right += 1
+            else:
+                if curr in count:
+                    return False
+                count.add(curr)
+                curr = 1
+                left = right
+                right += 1
+                
+        return True
+            
