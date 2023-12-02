97211501d76276bcc81c47332b4b2aa398564a9c
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-12-02 20:47:26 +0300

    Added solution for Linked List Cycle

---
+++ AfterCamp/linked-list-cycle.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        #brute force approach interms of space
+        store = set()
+        
+        while head:
+            if head in store:
+                return True
+            store.add(head)
+            head = head.next
+        return False
+        
