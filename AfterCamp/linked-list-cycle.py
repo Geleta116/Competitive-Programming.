755a0f275c2854fdbbd9d493fd87337662424286
Geleta Daba <104783472+Geleta116@users.noreply.github.com>
2023-12-03 23:15:45 +0300

    Added solution for Linked List Cycle

--- AfterCamp/linked-list-cycle.py
+++ AfterCamp/linked-list-cycle.py
@@ -9,15 +9,18 @@ class Solution:
         # kans algorithm
         if not head:
             return False
+        
         slow = head
         fast = head.next
         
         while fast and fast.next:
+            
             if fast == slow:
                 return True
             
             fast = fast.next.next
             slow = slow.next
+            
         return False
         
         
